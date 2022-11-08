23d93017b3f2253d9873ca147889f26f2d72e759
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-11-08 15:19:53 -0300

    feat: test mock

--- tests/create-challenge-submission.spec.ts
+++ tests/create-challenge-submission.spec.ts
@@ -1,12 +1,35 @@
 import { CreateChallengeSubmission } from "../src/app/Services/create-challenge-submission"
+import { Challenge } from "../src/domain/entities/Challenge"
+import { Student } from "../src/domain/entities/Student"
+import { InMemoryChallengesRepository } from "./repositories/in-memory-challenges-repository"
+import { InMemoryStudentsRepository } from "./repositories/in-memory-students-repository"
 
 describe('Create Challenge submission use case', () =>{
     it('should be able to create a new challenge submission', async () => {
-        const sut = new CreateChallengeSubmission();
+        const studentRepository = new InMemoryStudentsRepository()
+        const challengeRepository = new InMemoryChallengesRepository()
+
+        const student = Student.create({
+            name: "Arthur Isvi",
+            email: "spfcarthur1@gmail.com"
+        })
+
+        const challenge = Challenge.create({
+            title: "Challenge 1",
+            instructionsUrl: "http://example.com"
+        })
+
+        studentRepository.items.push(student);
+        challengeRepository.items.push(challenge);
+
+        const sut = new CreateChallengeSubmission(
+            studentRepository, 
+            challengeRepository
+        );
 
         const response = await sut.handle({
-            studentId: 'fake-student-id',
-            challengeId: 'fake-challenge-id',
+            studentId: student.id,
+            challengeId: challenge.id
 
         })
 
